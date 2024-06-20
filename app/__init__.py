5bcf30817697fa3d0d426fcab53ae4e289f493d9
sohumdalal <dsohum@gmail.com>
2024-06-20 14:24:41 -0400

    some config things

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,11 @@ import os
 
 def create_app():
     app = Flask(__name__)
+
     if os.environ.get('FLASK_ENV') == 'development':
         CORS(app)
+
     from app.api.routes import api_blueprint
     app.register_blueprint(api_blueprint)
+
     return app
--- app/utils/helper_functions.py
+++ app/utils/helper_functions.py
@@ -1,10 +1,14 @@
+# Utility functions like chunk_text, build_prompt, etc.
+
 PROMPT_LIMIT = 3750
 
 def chunk_text(text, chunk_size=200):
     # Split the text by sentences to avoid breaking in the middle of a sentence
     sentences = text.split('. ')
+    
     chunks = []
     current_chunk = ""
+
     for sentence in sentences:
         # Check if adding the next sentence exceeds the chunk size
         if len(current_chunk) + len(sentence) <= chunk_size:
@@ -13,25 +17,24 @@ def chunk_text(text, chunk_size=200):
             # If the chunk reaches the desired size, add it to the chunks list
             chunks.append(current_chunk)
             current_chunk = sentence + '. '
+
     # Add the last chunk if it's not empty
     if current_chunk:
         chunks.append(current_chunk)
+
     return chunks
 
-
 def build_prompt(query, context_chunks):
-    # create the start and end of the prompt
     prompt_start = (
-        "Answer the question based on the context below. If you don't know the answer based on the context provided below, just respond with 'I don't know' instead of making up an answer. Return just the answer to the question, don't add anything else. Don't start your response with the word 'Answer:'. Make sure your response is in markdown format\n\n"+
+        "Answer the question based on the context below. If you don't know the answer based on the context provided below, just respond with 'I don't know' instead of making up an answer. Don't start your response with the word 'Answer:'"
         "Context:\n"
     )
     prompt_end = (
         f"\n\nQuestion: {query}\nAnswer:"
     )
 
-    # append context chunks until we hit the 
-    # limit of tokens we want to send to the prompt.   
     prompt = ""
+    # append contexts until hitting limit
     for i in range(1, len(context_chunks)):
         if len("\n\n---\n\n".join(context_chunks[:i])) >= PROMPT_LIMIT:
             prompt = (
@@ -46,18 +49,4 @@ def build_prompt(query, context_chunks):
                 "\n\n---\n\n".join(context_chunks) +
                 prompt_end
             )
-    return prompt
-
-def construct_messages_list(chat_history, prompt):
-    messages = [{"role": "system", "content": "You are a helpful assistant."}]
-    
-    # Populate the messages array with the current chat history
-    for message in chat_history:
-        if message['isBot']:
-            messages.append({"role": "system", "content": message["text"]})
-        else:
-            messages.append({"role": "user", "content": message["text"]})
-    # Replace last message with the full prompt
-    messages[-1]["content"] = prompt    
-    
-    return messages
+    return prompt   
